e0a6e53d05af17442804072db956e73a0a31b6b0
eldin007b <begic.prodaja@gmail.com>
2026-04-27 20:01:11 +0200

    Auto-update: 2026-04-27 20:01:11

--- start.sh
+++ start.sh
@@ -19,29 +19,32 @@ git pull origin master
 
 while true; do
     node menu.js
-    
+
     if [ -f "$CMD" ]; then
         SCRIPT=$(cat "$CMD")
         rm -f "$CMD"
-        
-        # 1. OPCIJA: POTPUNO GAŠENJE (EXIT)
+
+        # 1. OPCIJA: POTPUNO GAŠENJE CIJELOG TERMUXA (EXIT)
         if [ "$SCRIPT" = "EXIT" ]; then
-            echo "Gasim i sinkroniziram..."
+            echo "Gasim sve i sinkroniziram..."
             sync_data
+            echo "Doviđenja!"
+            # Ova naredba gasi cijeli Termux proces
+            pkill -9 com.termux
             exit 0
         fi
 
-        # 2. OPCIJA: POVRATAK U TERMINAL (TERMINAL)
+        # 2. OPCIJA: POVRATAK U KONZOLU (TERMINAL)
         if [ "$SCRIPT" = "TERMINAL" ]; then
-            echo "Vraćam te u Termux..."
+            echo "Vraćam te u Termux terminal..."
             sync_data
-            break # Izlazi iz petlje, ostavlja te u folderu
+            break # Izlazi iz petlje, ostavlja te u folderu spreman za rad
         fi
-        
+
         echo "Pokrećem: $SCRIPT"
         node "$SCRIPT"
         sync_data
-        
+
         echo "Povratak za 3s..."
         sleep 3
     else
